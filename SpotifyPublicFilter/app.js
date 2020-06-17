0945cdffee5e79070fe5f8ca30a7955608b050cf
Senthil <spk2dc@virginia.edu>
2020-06-17 16:08:58 -0400

    searching spotify for albums and playlists based on user input completed

--- SpotifyPublicFilter/app.js
+++ SpotifyPublicFilter/app.js
@@ -1,39 +1,3 @@
-
-let basicTokenMethods = (token) => {
-    let baseurl = "https://api.spotify.com/v1/"
-    let testSongID = '4JjUwfp8GQ3PxWg2QPKnpn'
-    let filter = `${baseurl}audio-features/${testSongID}`
-    console.log(token);
-
-
-    $.ajax({
-        url: filter,
-        type: "GET",
-        data: {
-
-        },
-        headers: {
-            'Authorization': `${token.token_type} ${token.access_token}`
-        }
-
-    }).then((data) => {
-        console.log(data)
-    })
-
-}
-
-let searchPlaylists = (token) => {
-
-}
-
-let filter = (token) => {
-
-}
-
-let currentTrack = (token) => {
-
-}
-
 let getBasicToken = () => {
     let baseurl = "https://accounts.spotify.com/api/token"
     let encodedID = btoa(`${client_id}:${client_secret}`)
@@ -53,8 +17,84 @@ let getBasicToken = () => {
 
 }
 
+let basicTokenMethods = (token) => {
+    let baseurl = "https://api.spotify.com/v1/"
+    let testSongID = '4JjUwfp8GQ3PxWg2QPKnpn'
+    let filter = `${baseurl}audio-features/${testSongID}`
+
+    //log below is for testing ajax query using command prompt
+    // console.log(`curl -X "GET" "${filter}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: ${token.token_type} ${token.access_token}"`);
+
+    $.ajax({
+        url: filter,
+        type: "GET",
+        data: {
+
+        },
+        headers: {
+            'Authorization': `${token.token_type} ${token.access_token}`
+        }
+
+    }).then((data) => {
+        console.log(data);
+
+    })
+
+    $('#search-box').on('keypress', () => {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Cancel the default action, if needed
+            //event.preventDefault();
+            // Trigger the button element with a click
+            $('#search-button').click();
+        }
+    })
+    $('#search-button').on('click', (event) => {
+        event.preventDefault();
+        console.log(token);
+
+        searchPlaylists(token)
+
+    })
+
+}
+
+let searchPlaylists = (token) => {
+    let baseurl = "https://api.spotify.com/v1/search"
+    let queryStr = encodeURIComponent($('#search-box').val())
+    let typeStr = encodeURIComponent('playlist,album')
+    let limit = 10
+    let offset = 0
+    let finalurl = `${baseurl}?q=${queryStr}&type=${typeStr}&limit=${limit}&offset=${offset}`
+    //log below is for testing ajax query using command prompt
+    //console.log(`curl -X "GET" "${baseurl}?q=${queryStr}&type=${typeStr}&limit=10&offset=5" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: ${token.token_type} ${token.access_token}"`);
+    
+
+    $.ajax({
+        type: "GET",
+        url: finalurl,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${token.token_type} ${token.access_token}`,
+        }
+
+    }).then((data) => {
+        console.log(data)
+    })
+}
+
+let filter = (token) => {
+
+}
+
+let currentTrack = (token) => {
+
+}
+
 $(() => {
     getBasicToken()
+
 
 });
 
@@ -87,5 +127,7 @@ https://towardsdatascience.com/step-by-step-to-visualize-music-genres-with-spoti
 https://stackoverflow.com/questions/41224070/spotify-javascript-api-getting-the-genre-of-a-song-in-a-playlist
 
 https://github.com/watsonbox/exportify
+
+https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 
 */

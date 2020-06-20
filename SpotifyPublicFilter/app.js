4a7c193d91ce3ecc8ba1df6ea89cf26f22d778b3
Senthil <spk2dc@virginia.edu>
2020-06-20 05:40:06 -0400

    hmm still not working, not sure if the live site just hasn't updated with the latest code push yet. adding parentheses just in case cause why not. ok now i'm going to bed haha

--- SpotifyPublicFilter/app.js
+++ SpotifyPublicFilter/app.js
@@ -485,7 +485,7 @@ $(() => {
     //if client and secret ids already exist in environment variables then go straight to token method and reassign click event
     $('#search-button').on('click', (event) => {
         event.preventDefault();
-        if (typeof client_id === 'string' && typeof client_secret === 'string') {
+        if (typeof (client_id) === 'string' && typeof (client_secret) === 'string') {
             clientID = client_id
             secretID = client_secret
             getBasicToken(clientID, secretID, event)
